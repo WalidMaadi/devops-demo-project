206a68ec13c05c439ebcf29f9c6d424ccc449004
WalidMaadi <167113119+WalidMaadi@users.noreply.github.com>
2025-06-01 17:21:24 +0200

    Update DemoApplication.java

--- demo-java-app/src/main/java/com/deepak/demo/DemoApplication.java
+++ demo-java-app/src/main/java/com/deepak/demo/DemoApplication.java
@@ -37,6 +37,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Version modifiée");
     }
 
 }
